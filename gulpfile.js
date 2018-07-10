adfd23da068b76654ec7b4ddf788d0260a5c8b25
mizuki.kawano <mizuki.kawano@nijibox.co.jp>
2018-07-10 19:01:52 +0900

    add: gulp img-min and header nav

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+
+// gulp.task('images', function(){
+//   return gulp.src('dev/images/**/*.+(png|jpg|gif|svg)')
+//   .pipe(imagemin())
+//   .pipe(gulp.dest('dist/images'));
+// });
+
+gulp.task('images', function(){
+  return gulp.src('dev/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('task-name', function(callback) {
+  runSequence('task-one', 'task-two', 'task-three', callback);
+});
+
+gulp.task('default', function (callback) {
+  runSequence('clean:dist', 'images',
+    callback
+  );
+});
--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
-function navOpen() {
-		$(".js-navopen, .js-navclose").click(function(e){
-			e.preventDefault();
-			if($(".menu-icon").hasClass("is-open")){
-				$(".menu-icon").removeClass("is-open");
-				$(".menu-icon_overlay").addClass("menu-icon_overlay-hide");
-			}else{
-				$(".menu-icon").addClass("is-open");
-				$(".menu-icon_overlay").removeClass("menu-icon_overlay-hide");
-			}
-		});
-		$(".menu-icon_list li").each(function(i, elem) {
-			var $btn = $(this).find("a");
-			$btn.click(function(){
-				$(".menu-icon").removeClass("is-open");
-				$(".menu-icon_overlay").addClass("menu-icon_overlay-hide");
-			});
-		});
-		$(".menu-icon_item_contact a").click(function(e){
-			e.preventDefault();
-			gtag('event', ' click', {'event_category': 'click_contact','event_label': 'header_contact_sp'});
-			$(".menu-icon").removeClass("is-open");
-			$(".menu-icon_overlay").addClass("menu-icon_overlay-hide");
-		});
-	}
+function openNav() {
+
+  if($(".site-nav").hasClass("is-open")){
+    $(".site-nav").removeClass("is-open");
+    $(".site-title").addClass("is-hidden");
+  }else{
+    $(".site-nav").addClass("is-open");
+    $(".site-title").removeClass("is-hidden");
+  }
+
+  $(".site-nav_item_contact a").click(function(e){
+    e.preventDefault();
+    $(".site-nav").removeClass("is-open");
+  });
+
+}
+
+function closeNav() {
+  if($(".site-nav").hasClass("is-open")){
+    $(".site-nav").removeClass("is-open");
+    $(".site-title").addClass("is-hidden");
+  }else{
+    $(".site-nav").addClass("is-open");
+    $(".site-title").removeClass("is-hidden");
+  }
+}
